fdaedbe28399131e84d5ad00ec07c422af93da49
Omid Foroqi <foroqi@protonmail.com>
2021-10-16 14:31:09 +0330

    fix database connect and script flow, add comment

--- production/labeling-server/labeling_ui/label_ui.py
+++ production/labeling-server/labeling_ui/label_ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 from labeldb import Labeling
 from decouple import config
 
-# import `.env` variables 
+# import `.env` variables
 DB_HOST = config('LABEL_DB_HOST')
 DB_NAME = config('POSTGRES_LABEL_DATABASE')
 DB_USER = config('POSTGRES_USER')
@@ -17,44 +17,53 @@ st.set_page_config(
     page_title="Labeling", layout="wide",
 )
 # SET page title
-page_title = "<h1 style='text-align: center;'>label</h1>"
+page_title = "<h1 style='text-align: center;'>Labeling Service</h1>"
 st.markdown(page_title, unsafe_allow_html=True)
-if 'feedback_value' not in st.session_state:
-    st.session_state.feedback_value = False
+
 
 def submit_feedback():
+    """function for write a record when submit button clicked"""
     labeling.write_record(
-        st.session_state.username, 
-        st.session_state.summary,
+        st.session_state.username,
         st.session_state.feedback_value,
-        st.session_state.user_summary if st.session_state.user_summary !="" else None,
-        )
+        st.session_state.user_summary if st.session_state.user_summary != "" else None,
+        st.session_state.unique_id,
+    )
 
+
+# add feedback value in session state
+if 'feedback_value' not in st.session_state:
+    st.session_state.feedback_value = False
 # user take username and check is valid or not
 if 'login_result' not in st.session_state:
     st.text_input("Username", key="username")
     st.text_input("Password", key="passwd", max_chars=12, type='password')
     if st.button('Login'):
-        st.session_state.login_result = labeling.login_user(st.session_state.username, st.session_state.passwd)
-elif st.session_state.login_result:
-    st.success(f"Welcome {st.session_state.username}")
-    st.session_state.context, st.session_state.summary = labeling.give_record(st.session_state.username)
-    st.subheader("Context")
-    st.write(st.session_state.context)
-    st.subheader("Summary")
-    st.write(st.session_state.summary)
-    with st.form("feedack_form"):
-        st.subheader("Is this summary correct?")
-        feedback_radio = st.radio("Is the summary correct?", ("True", "False"))
-        st.session_state.feedback_value = True if feedback_radio == "True" else False
-        st.text_area("if the summary isn't correct please write correct summary", key='user_summary')
-        subimt = st.form_submit_button("Submit", on_click=submit_feedback)
+        st.session_state.login_result = labeling.login_user(
+            st.session_state.username, st.session_state.passwd)
+if 'login_result' in st.session_state:
+    # show article from database if username and password are correct
+    if st.session_state.login_result:
+        # Welcome message
+        st.success(f"Welcome {st.session_state.username}")
+        st.session_state.context, st.session_state.summary, st.session_state.unique_id = labeling.give_record(
+            st.session_state.username)
+        # Show retrieved context and summary for user
+        st.subheader("Context")
+        st.write(st.session_state.context)
+        st.subheader("Summary")
+        st.write(st.session_state.summary)
 
-else:
-    st.warning(f"login not correct, Try again after refrash page")
-    del st.session_state.login_result
-
-
-# add user to a session 
-# if user not in st.session_state:
-#     st.session_state['user'] = user
+        with st.form("feedack_form"):
+            st.subheader("Is this summary correct?")
+            feedback_radio = st.radio(
+                "Is the summary correct?", ("True", "False"))
+            # Convert radio answer to bool value
+            st.session_state.feedback_value = True if feedback_radio == "True" else False
+            st.text_area(
+                "if the summary isn't correct please write correct summary", key='user_summary')
+            subimt = st.form_submit_button("Submit", on_click=submit_feedback)
+    else:
+        # Show if username or password is wrong
+        st.warning(f"login not correct, Try again after refrash page")
+        del st.session_state.login_result
